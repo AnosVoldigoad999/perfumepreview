84f5eaa13411fb69d7ed92d5a64c63c7fde0b4ef
AnosVoldigoad999 <oyedejiuzaymah@gmail.com>
2023-08-26 13:10:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+
+export default function App (){
+  return <>
+ <div className='container'> 
+<div className='pic' />
+
+<div className='everything'>
+<p className='perf'>PERFUME</p>
+<h1>Gabrielle Essence Eau De Parfum</h1>
+<p className='desc'>
+  A floral, solar and voluptous interpretation composed by Olivier Polge, Perfumer-Creator for the house of CHANEL
+</p>
+
+  <h2>$149.99 <s>$169.99</s></h2>
+  <button id='butt' ><img alt='icon-cart' src='./public/icon-cart.svg'></img>Add to cart</button>
+</div>
+
+ </div>
+
+  </>
+}
