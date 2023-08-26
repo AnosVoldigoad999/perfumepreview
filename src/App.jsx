6450f584c445a0e84e27c9e4abc189adcc97d0f1
AnosVoldigoad999 <oyedejiuzaymah@gmail.com>
2023-08-26 14:00:41 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export default function App (){
 </p>
 
   <h2>$149.99 <s>$169.99</s></h2>
-  <button id='butt' ><img alt='icon-cart' src='./icon-cart.svg' width={50} height={50} ></img>Add to cart</button>
+  <button id='butt' ><img alt='icon-cart' src='./icon-cart.svg' />Add to cart</button>
 </div>
 
  </div>
